bee5a8b69f38918ef7ef49ecc5e6191b9ead8e43
Gandoki <abahgideon111@gmail.com>
2024-07-13 17:00:21 -0700

    logic optimise

--- src/App.vue
+++ src/App.vue
@@ -59,6 +59,8 @@ const resetQuiz = () => {
   <div class="flex flex-col items-center justify-center h-full">
     <h1 class="text-3xl text-center p-3 w-full bg-white text-black rounded">Nigerian Quiz</h1>
     <p v-if="!completed" class="text-center p-3 w-full mt-3">Carefully select the correct answer:</p>
+   <span>{{ currentQuestionIndex.question }}</span>
+   <span>Question: {{ currentQuestionIndex }} of {{ questions.length }}</span>
     <div v-if="!completed" class="flex flex-col gap-4 mt-5">
       <h2 class="text-lg text-center">{{ currentQuestion.question }}</h2>
       <div class="flex flex-col gap-2">
@@ -76,8 +78,12 @@ const resetQuiz = () => {
         Next Question
       </button>
     </div>
-    <div v-else class="flex flex-col gap-4">
+    <div v-else class="flex flex-col gap-4 border-white border mt-5 shadow-white rounded p-8">
       <h2 class="text-lg text-center">Quiz Completed!</h2>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
+      </svg>
+
       <p class="text-lg text-center">Your score: {{ correctAnswers }}</p>
       <button @click="resetQuiz" class="bg-white text-black py-2 px-4 rounded-md text-sm">
         Reset Quiz
